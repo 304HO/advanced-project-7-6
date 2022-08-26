cf3c03135aee05eaab7ff164b74a986fa2b641c1
meanjeful <meanjeful@gmail.com>
2022-08-26 18:11:56 +0900

    FIX: fix step

--- src/components/Step.jsx
+++ src/components/Step.jsx
@@ -1,10 +1,14 @@
 import styled from "styled-components";
+import { Tabs } from "antd";
+import React from "react";
+const { TabPane } = Tabs;
 
 const Container = styled.div`
   display: flex;
   flex-direction: row;
   justify-content: center;
   align-items: center;
+  gap: 10px;
 `;
 
 const Tab = styled.div``;
@@ -12,9 +16,11 @@ const Tab = styled.div``;
 function Step({ submitData, page, setPage }) {
   return (
     <Container>
-      {submitData.map((item, index) => {
-        return <Tab key={index}>{index + 1}</Tab>;
-      })}
+      <Tabs defaultActiveKey="1" centered>
+        {submitData.map((item, index) => {
+          return <TabPane key={index}>{index + 1}</TabPane>;
+        })}
+      </Tabs>
     </Container>
   );
 }
